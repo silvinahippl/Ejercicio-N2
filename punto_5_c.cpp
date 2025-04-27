a0e829c68bda7809e4f9b7c416892d5bfd428134
Silvina <msilvinahippler@gmail.com>
2025-04-27 03:16:22 -0300

    punto_5_c.cpp

--- punto_5_c.cpp
+++ punto_5_c.cpp
@@ -1,10 +1,12 @@
 //c) Imprimir en pantalla los números de 1 a 16 y sus valores al cuadrado al lado (1 – 1, 2 – 4, 3 – 9, 4 – 16, 5 – 25, etc.). Este ejercicio es igual que el hecho anteriormente con while pero utilizando el ciclo for//
 
 #include <iostream>
+using namespace std;
 
 int main() 
 {
-    for (int i = 1; i <= 16; ++i) {
+    for (int i = 1; i <= 16; ++i) 
+{
         int cuadrado = i * i;
     cout << i << " - " << cuadrado << endl;
     }
