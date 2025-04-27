6eec4c38a4ab9c6246192a6bc2e294784f501f60
Silvina <msilvinahippler@gmail.com>
2025-04-27 01:47:37 -0300

    punto_7_c.cpp

---
+++ punto_7_c.cpp
@@ -0,0 +1,15 @@
+//Escribir programas con ciclos while para: c) Imprimir en pantalla los números de 2 al 20 incrementando de a dos (2, 4, 6, 8, etc.)
+
+#include <iostream>
+using namespace std;
+
+int main()
+int numero = 2;
+
+while (numero <=20) //Se ejecuta mientras num sea igual o menos a 20
+{
+cout <<numero <<" "; //" " para espacio entre numeros
+numero = numero + 2;
+}
+return 0;
+}
